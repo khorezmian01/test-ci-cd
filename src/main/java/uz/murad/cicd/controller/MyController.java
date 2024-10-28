c3be09c81606c3b5976a1327fc07298cc75da42c
HP <sultanbaevm2001@gmail.com>
2024-10-28 09:21:51 +0500

    2

---
+++ src/main/java/uz/murad/cicd/controller/MyController.java
@@ -0,0 +1,18 @@
+package uz.murad.cicd.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+@RequiredArgsConstructor
+public class MyController {
+
+
+    @GetMapping
+    public String hello() {
+        return "Hello World";
+    }
+}
